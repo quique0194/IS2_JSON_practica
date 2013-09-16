a30871e5b9a2f56cbbaeac7bcf9b9159a2d26188
Stephany Jurado <stephy1705@gmail.com>
2013-09-15 22:41:28 -0500

    main

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,24 @@ public class Main {
 		
 		System.out.println(a1.toJSON());
 		
+		 Matricula m1 = new Matricula();
++    
++    m1.setIdAlumno(1111023149L);
++    m1.setIdCurso(742L);
++    m1.setNota(15.5);
++    
++    System.out.println(m1.toJSON());
+
+  Curso c1 = new Curso();
++    Profesor p1 = new Profesor();
++    
++    c1.setId(1L);
++    c1.setNombre("Teolog?a");
++    c1.setIdCursoSgt(2L);
++    c1.setProfesor(p1);
++    
++    System.out.println(c1.toJSON());
+
 		Horario h1 = new Horario();
 		h1.setId(1454545L);
 		h1.setDia(1);
