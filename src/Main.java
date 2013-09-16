2fb8cc95aec08c40858cdd714a4f1945b900bd36
Miguel Bustamante <miguel.bustamante@ucsp.edu.pe>
2013-09-15 19:30:05 -0500

    matricula terminado

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,14 @@ public class Main {
 		a1.setApellidos("Carrillo Pino");
 		
 		System.out.println(a1.toJSON());
+		
+		Matricula m1 = new Matricula();
+		
+		m1.setIdAlumno(1111023149L);
+		m1.setIdCurso(742L);
+		m1.setNota(15.5);
+		
+		System.out.println(m1.toJSON());
 
 	}
 	
--- src/Matricula.java
+++ src/Matricula.java
@@ -1,4 +1,55 @@
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class Matricula {
+	
+	private Long idAlumno;
+	private Long idCurso;
+	private Double nota;
+	
+	public Long getIdAlumno() {
+		return idAlumno;
+	}
+	public void setIdAlumno(Long i) {
+		this.idAlumno = i;
+	}
+	public Long getIdCurso() {
+		return idCurso;
+	}
+	public void setIdCurso(Long idCurso) {
+		this.idCurso = idCurso;
+	}
+	public Double getNota() {
+		return nota;
+	}
+	public void setNota(Double nota) {
+		this.nota = nota;
+	}
+	
+	public String toJSON()
+	{
+		String ret="{\n";
+		List<Field> fields = Arrays.asList(this.getClass().getDeclaredFields());
+		for(Field field:fields)
+		{
+			String atributo = field.getName();
+			String value="";
+			try {
+				value = field.get(this).toString();
+			} catch (IllegalArgumentException | IllegalAccessException e) {
+				e.printStackTrace();
+			}
+			if( field.getType() == String.class )
+			{
+				value = "\"" + value + "\"";
+			}
+			ret = ret + "\t\"" + atributo + "\"" + ": " + value +",\n";
+		}
+		ret = ret.substring(0, ret.length()-2);
+		ret = ret + "\n};";
+		return ret;
+	}
 
 }
